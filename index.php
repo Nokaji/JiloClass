615e30cd46d05572db2465a1eef4cc8f43bb4a0d
Nokaji <oximas.yt@gmail.com>
2022-05-22 19:58:12 +0200

    update

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil | JiloClass</title>
-    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/index.css">
 </head>
 <body>
+<header>
+
+</header>
 <?php include 'src/includes/navbar.php';?>
 </body>
 <script src="src/js/navbar.js"></script>
