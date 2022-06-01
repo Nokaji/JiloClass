97fba1f36c4d2c639159a891110e22cc3be4543f
Nokaji <oximas.yt@gmail.com>
2022-06-01 18:55:30 +0200

    update

--- index.php
+++ index.php
@@ -16,25 +16,23 @@ include ''.BASE_URL.'src/includes/database.php';
 <body>
     <?php include 'src/includes/navbar.php';?>
     <header>
-        <h1>
-            jiloClass
-        </h1>
-        <p>venez enseigner/apprendre de l'informatique</p>
+        <img src="src/img/jiloclass.png"/>
+        <p>venez enseigner/apprendre l'informatique</p>
     </header>
     <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-<defs>
-<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-</defs>
-<g class="parallax">
-<use xlink:href="#gentle-wave" x="48" y="7" fill="#303030" />
-</g>
-</svg>
+        viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <defs>
+        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g class="parallax">
+        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255, 255, 255, 0.227)" />
+        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255, 255, 255, 0.447)" />
+        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255, 255, 255, 0.6)" />
+        <use xlink:href="#gentle-wave" x="48" y="7" fill="white" />
+        </g>
+    </svg>
     <div class="section-hqp-1">
-        <div class="icons-colomns">
-        <i class="fa-solid fa-graduation-cap"></i>
-        <p>Apprennez facilement avec JiloClass</p>
-        </div>
+        
     </div>
 </body>
 <script src="src/js/navbar.js"></script>
